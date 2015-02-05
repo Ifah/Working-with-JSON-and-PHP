af8702c0afbd3ca659e640f6957a894d948388d3
Mohamed Ifah <ifaa.x.h@gmail.com>
2015-02-06 00:25:32 +0500

    Working with JSON and PHP
    This video introduces working with JSON and PHP together, using the
    built-in json_encode function. In this tutorial, we create a short
    example of a calculator to submit, request and add two numbers together.
    We ajax for the results and return them in valid JSON format.

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>JSON and PHP</title>
+</head>
+<body>
+	<form action="add.php" method="post" id="add">
+		<input type="text" name="first"> +
+		<input type="text" name="second">
+		<input type="submit" value="Work this out">
+	</form>
+
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/global.js"></script>
+</body>
+</html>
